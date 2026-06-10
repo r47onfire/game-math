827a479c4bf0ca5a0664d1348c0a8b6c5fa27ece
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-06-09 23:07:22 -0400

    implement polygon random

--- src/geometry/shape/ops/random.ts
+++ src/geometry/shape/ops/random.ts
@@ -1,6 +1,7 @@
-import { sqrt } from "lib0/math";
+import { abs, sqrt } from "lib0/math";
 import { Vec2, Vec2_addScaled_m, Vec2_clone, Vec2_fromAngle, Vec2_lerp } from "../../../linearAlgebra";
-import { Random_floatBelow, RandomSource } from "../../../random";
+import { Random_floatBelow, Random_indexWeighted, RandomSource } from "../../../random";
+import { triangulate } from "../../triangulate";
 import { Point } from "../Point";
 import { Shape, ShapeClassForType, ShapeType } from "../Shape";
 
@@ -21,14 +22,30 @@ export const Line_random: RandomDispatchTable[ShapeType.LINE] = (line, rng) => r
 
 export const Point_random = (p: Point) => Vec2_clone(p.pt);
 
+const triangle_area = (p1: Vec2, p2: Vec2, p3: Vec2) => {
+    const ax = p2.x - p1.x, ay = p2.y - p1.y;
+    const bx = p3.x - p1.x, by = p3.y - p1.y;
+    return abs(ax * by - ay * bx) / 2;
+}
+const triangle_random = (p1: Vec2, p2: Vec2, p3: Vec2, r1: number, r2: number) => {
+    // https://math.stackexchange.com/q/18686
+    const w1 = 1 - sqrt(r1);
+    const w2 = sqrt(r1) * (1 - r2);
+    const w3 = r2 * sqrt(r1);
+    return new Vec2(
+        p1.x * w1 + p2.x * w2 + p3.x * w3,
+        p1.y * w1 + p2.y * w2 + p3.y * w3,
+    );
+}
 export const Polygon_random: RandomDispatchTable[ShapeType.POLYGON] = (polygon, rng) => {
-    /**
-     * TODO:
-     * - cut into triangles
-     * - choose a random triangle weighted on the triangles' areas
-     * - choose a random point in the triangle
-     */
-    return Vec2_clone(polygon.pts[0]);
+    const pts = polygon.pts;
+    const tri = triangulate(pts);
+    const weights: number[] = [];
+    for (var i = 0; i < tri.length; i += 3) {
+        weights.push(triangle_area(pts[tri[i]!]!, pts[tri[i + 1]!]!, pts[tri[i + 2]!]!));
+    }
+    const t = 3 * Random_indexWeighted(rng, weights);
+    return triangle_random(pts[tri[t]!]!, pts[tri[t + 1]!]!, pts[tri[t + 2]!]!, rng(), rng());
 }
 
 export const Rect_random: RandomDispatchTable[ShapeType.RECTANGLE] = (rect, rng) =>
