5641629f1b45c2c89cc017e6eb9fd9300ac80f57
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-07-06 16:11:23 -0400

    add issubclass

--- src/common.ts
+++ src/common.ts
@@ -13,6 +13,10 @@ export const isinstance = <C>(obj: any, cls: abstract new (...args: any[]) => C)
     return obj instanceof cls;
 }
 
+export const issubclass = <A, B>(sub: abstract new (...args: any[]) => A, super_: abstract new (...args: any[]) => B): A extends B ? true : B extends A ? false : boolean => {
+    return sub.prototype instanceof super_ as any;
+}
+
 const idMap = new WeakMap<object, number>();
 var idCounter = 0;
 export const id = (obj: object): number => {
