cb5675d90d38c40dba77ff7993612048027b1164
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-08-31 15:04:54 -0400

    i hate you vscode

--- test/dsa/sort.test.ts
+++ test/dsa/sort.test.ts
@@ -1,18 +1,13 @@
-import { test, expect } from "bun:test";
-import { insertionSort } from "lib0/sort.js";
-import { Random_choice } from "../../src";
+import { expect, test } from "bun:test";
+import { insertionSort, Random_choice } from "../../src";
 
 const list = Array.from({ length: 1000 }, (_, i) => i);
 
 test.each([
     ["insertion sort", insertionSort]
 ])("%s", (_, f) => {
-    for (var i = 0; i < 10000; i++) {
-        const items = Array.from({ length: 10000 }, () => ({ foo: Random_choice(Math.random, list) }));
-        const realSorted = items.toSorted((a, b) => a.foo - b.foo);
-        f(items, (a, b) => a.foo - b.foo);
-        for (var i = 0; i < items.length; i++) {
-            expect(realSorted[i]).toBe(items[i]);
-        }
-    }
+    const items = Array.from({ length: 10000 }, (_, i) => ({ n: Random_choice(Math.random, list), i }));
+    const realSorted = items.toSorted((a, b) => a.n - b.n);
+    f(items, (a, b) => a.n - b.n);
+    expect(items.map(x => x.i)).toEqual(realSorted.map(x => x.i));
 });
