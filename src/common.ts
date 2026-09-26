68f45da727dcc6e4badafc2ee3f62fd6f90fe11b
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-09-26 11:11:32 -0400

    remove isinstance

--- src/common.ts
+++ src/common.ts
@@ -9,10 +9,6 @@ export const asin = Math.asin;
 export const acos = Math.acos;
 export const atan = Math.atan;
 
-export const isinstance = <C>(obj: any, cls: abstract new (...args: any[]) => C): obj is C => {
-    return obj instanceof cls;
-}
-
 export const issubclass = <A, B>(sub: abstract new (...args: any[]) => A, super_: abstract new (...args: any[]) => B): A extends B ? true : B extends A ? false : boolean => {
     return sub.prototype instanceof super_ as any;
 }
